49c35416445d0c2afa6f40946d84659ba92b96d5
tetpic <rabustes@gmail.com>
2023-07-04 21:27:36 +0300

    add some backend blogs endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require( "express" );
 // import express from "express"
 const app = express();
 const port = 8080; // default port to listen
+const urlencodedParser = express.urlencoded({extended: false});
 
 const mysql = require("mysql");
   
@@ -11,31 +12,63 @@ const connection = mysql.createConnection({
   database: "tetpic",
   password: "password"
 });
- connection.connect(function(err){
+connection.connect(function(err){
     if (err) {
       return console.error("Ошибка: " + err.message);
     }
     else{
       console.log("Подключение к серверу MySQL успешно установлено");
     }
- });
-
-// define a route handler for the default home page
-app.get( "/api", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-
-app.get( "/api/blog", ( req, res ) => {
-    connection.query("SELECT * FROM blog",
-  function(err, results, fields) {
-    console.log(err);
-    console.log(results); // собственно данные
-    // console.log(fields); // мета-данные полей 
 });
-} );
+
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+  console.log( `server started at http://localhost:${ port }` );
 } );
+
+
+app.get( "/api/blogs", ( req, res ) => {
+  connection.query("SELECT * FROM tetpic.blog;", (err, data)=>{
+    res.send( JSON.stringify(data) );
+  })
+} );
+
+app.post("/api/blogs", urlencodedParser, (req, res)=> {
+  if(!req.body) return res.sendStatus(400);
+  let {title, body} = req.body
+  let created = new Date()
+  connection.query("INSERT INTO tetpic.blog (title, body, created) VALUES (?, ?, ?);", [title, body, created], (err, data)=> {
+    if(err) {
+      res.send(err)
+      return
+    }
+    res.send(JSON.stringify({status: "ok"}))
+  })
+})
+
+app.patch("/api/blogs", urlencodedParser, (req, res)=> {
+  if(!req.body) return res.sendStatus(400)
+  console.log(req.body)
+  let {title, body, id} = req.body
+  connection.query(`UPDATE tetpic.blog SET title = '${title}', body = '${body}' WHERE id = ${id};`, (err)=> {
+    if (err) {res.send(err); return}
+    res.send(JSON.stringify({status: "updated"}))
+  })
+})
+
+app.delete("/api/blogs", urlencodedParser, (req, res)=> {
+  if(!req.body) return res.sendStatus(400)
+  let {id} = req.body
+  connection.query(`DELETE FROM tetpic.blog WHERE (id = ${id});`, (err)=> {
+    if(err) {res.send(err); return}
+    res.send(JSON.stringify({status: "deleted"}))
+  })
+})
+
+
+
+
+
+
 
